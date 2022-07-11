c4e5b83c953ac03a38123d331a3495fc7f3b4a30
damian-is <94235867+damian-is@users.noreply.github.com>
2022-07-11 20:24:37 +0200

    learnign_the_very_basics

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import flask
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'Hello World! <h5>HELLO<h5>'
+
+@app.route("/<name>")
+def user(name):
+    return f'Hello {name}'
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == '__main__':
+    app.run()
